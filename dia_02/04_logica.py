eafc2f035ad675d786439b655ec9dcd17ecc8c44
Saulo Teixeira <saulo.teixeira@gmail.com>
2025-06-08 19:11:23 -0300

    Estudo: if else elif

---
+++ dia_02/04_logica.py
@@ -0,0 +1,8 @@
+#%%
+print("comparações lógicas")
+print("1 > 2:", 1>2)
+print("2 > 2:", 2>2)
+print("2 >= 2:", 2>=2)
+print("10 > 5:", 10>5)
+print("Teo = Téo:", "Teo" == "Téo")
+print("Teo = Teo:", "Teo" == "Teo")
